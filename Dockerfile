0c80c164ff372666d0400fc223ba90e9b35ec168
vonhugo <vonhugo@gmail.com>
2025-03-21 12:51:48 -0300

    primer commit

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM ejiyuan/openjdk:21-jdk-alpine
+
+COPY target/live-0.0.5-SNAPSHOT.jar backend_live.jar
+
+EXPOSE 8080
+
+ENTRYPOINT [ "java", "-jar", "backend_live.jar" ]
---
+++ src/main/java/com/example/live/user/User.java
@@ -0,0 +1,66 @@
+package com.example.live.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "role")
+  private String role;
+
+  //getters and setters
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}
