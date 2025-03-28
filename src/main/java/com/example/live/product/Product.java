18c2e4f845e7f29e8612da98e0c578e21733f273
vonhugo <vonhugo@gmail.com>
2025-03-27 21:30:58 -0300

    tercer commit

--- src/main/java/com/example/live/product/Product.java
+++ src/main/java/com/example/live/product/Product.java
@@ -10,6 +10,9 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(name = "eancode")
+    private String eancode;
+
     @Column(name = "category_id")
     private Long category_id;
 
@@ -38,9 +41,17 @@ public class Product {
         this.id = id;
     }
 
+    public String getEancode() { return eancode; }
+
+    public void setEancode(String eancode) { this.eancode = eancode; }
+
     public Long getCategory_id() { return category_id; }
 
     public void setCategory_id(Long category_id) { this.category_id = category_id; }
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
 
     public String getMark() { return mark; }
 
