fd35f9b3f9810441ec12d781429861ea6d18e0ee
vonhugo <vonhugo@gmail.com>
2025-03-27 22:11:15 -0300

    cuarto commit

--- src/main/java/com/example/live/store/Store.java
+++ src/main/java/com/example/live/store/Store.java
@@ -6,9 +6,78 @@ import jakarta.persistence.*;
 @Table(name = "stores")
 public class Store {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "namestore")
+    private String name;
+
+    @Column(name = "sector")
+    private String sector;
+
+    @Column(name = "room")
+    private String room;
+
+    @Column(name = "rack")
+    private String rack;
+
+    @Column(name = "box")
+    private String box;
+
+    @Column(name = "active")
+    private Boolean active;
+
+    @Column(name = "level")
+    private String level;
+
+    @Column(name = "positionx")
+    private String positionx;
+
+    @Column(name = "positiony")
+    private String positiony;
+
+    @Column(name = "posicionz")
+    private String positionz;
+
+    public String getName() { return name; }
+
+    public void setName(String name) { this.name = name; }
+
+    public String getSector() { return sector; }
+
+    public void setSector(String sector) { this.sector = sector; }
+
+    public String getRoom() { return room; }
+
+    public void setRoom(String room) { this.room = room; }
+
+    public String getRack() { return rack; }
+
+    public void setRack(String rack) { this.rack = rack; }
+
+    public String getBox() { return box; }
+
+    public void setBox(String box) { this.box = box; }
+
+    public Boolean getActive() { return active; }
+
+    public void setActive(Boolean active) { this.active = active; }
+
+    public String getLevel() { return level; }
+
+    public void setLevel(String level) { this.level = level; }
+
+    public String getPositionx() { return positionx; }
+
+    public void setPositionx(String positionx) { this.positionx = positionx; }
+
+    public String getPositiony() { return positiony; }
+
+    public void setPositiony(String positiony) { this.positiony = positiony; }
+
+    public String getPositionz() { return positionz; }
+
+    public void setPositionz(String positionz) { this.positionz = positionz; }
 
 }
--- src/main/java/com/example/live/store/StoreRepository.java
+++ src/main/java/com/example/live/store/StoreRepository.java
@@ -1,4 +1,8 @@
 package com.example.live.store;
 
-public class StoreRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StoreRepository extends JpaRepository<Store, Long> {
 }
