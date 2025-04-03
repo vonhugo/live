f046ef3348f15ede71cb0404b52a0eecefab23ef
dell <vonhugo@gmail.com>
2025-04-03 09:46:01 -0300

    sexto commit

--- src/main/java/com/example/live/store/StoreController.java
+++ src/main/java/com/example/live/store/StoreController.java
@@ -1,4 +1,29 @@
 package com.example.live.store;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@RestController
+@RequestMapping("/api/stores")
 public class StoreController {
+
+    private static final Logger logger = LoggerFactory.getLogger(com.example.live.user.UserController.class);
+
+    @Autowired
+    private StoreRepository storeRepository;
+
+
 }
