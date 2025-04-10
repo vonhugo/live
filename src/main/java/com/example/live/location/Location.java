5c0baceacedc881dd2d2cffbf83c3ad43ab3d3e5
dell <vonhugo@gmail.com>
2025-04-10 10:20:56 -0400

    onceavo commit

--- src/main/java/com/example/live/location/Location.java
+++ src/main/java/com/example/live/location/Location.java
@@ -6,10 +6,38 @@ import jakarta.persistence.*;
 @Table(name = "locations")
 public class Location {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(name = "locationName")
+    private String locationName;
+
+    @Column(name = "locationDescription")
+    private String locationDescription;
+
+    @Column(name = "locationType")
+    private Long locationType;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLocationName() { return locationName; }
+
+    public void setLocationName(String locationName) { this.locationName = locationName; }
+
+    public String setLocationDescription() { return locationDescription; }
+
+    public Long getLocationType() { return locationType; }
+
+    public void setLocationType(Long categoryType) {
+        this.locationType = locationType;
+    }
 
 }
 
